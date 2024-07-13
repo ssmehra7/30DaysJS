2fd7a83535722b81399c26ea6a19197c4ce056c6
simarjeet <simarjeet.sm7@gmail.com>
2024-07-14 03:25:43 +0530

    completed day1

---
+++ day1.js
@@ -0,0 +1,58 @@
+// //task1 
+
+// var a = 2; 
+// console.log(a,"From var variable");
+
+// let b = 2; 
+// console.log(b,"From let variable");
+
+// const c = true;
+// console.log(c, "boolean value from c ");
+
+// const number_var = 2; 
+// const string_var = "Hello";
+// const boolean_var = true;
+// const object_var = {
+//     name :"Simarjeet",
+//     sex:"male",
+// }
+
+// const array_var=[1,3,4,5];
+
+// console.log("number_var type is",typeof(number_var));
+// console.log("string_var type is",typeof(string_var));
+// console.log("boolean_var type is",typeof(boolean_var));
+// console.log("object_var type is",typeof(object_var));
+
+
+// let x = 2; 
+// console.log("Value of x before reassigning -->",x);
+// x = 5; 
+// console.log("Value of x after reassigning -->",x);
+
+
+
+// function typeofvariable(x){
+//     console.log("Type of variable is ",typeof(x));
+//     console.log("Value of variable is",x);
+// }
+
+// typeofvariable("hello");
+// typeofvariable(2);
+
+//let vs const 
+
+let y = 2 ; 
+console.log("Value of x before reassigning -->",y);
+y = 5 ; 
+console.log("Value of x after reassigning -->",y);
+
+const z = true; 
+
+try{
+    z = false;
+}catch(e){
+    console.log(e);
+}
+
+
